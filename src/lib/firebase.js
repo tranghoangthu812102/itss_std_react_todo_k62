7e383303644f75f25015bbfde01e3f670c4d4a66
EC2 Default User <ec2-user@ip-172-31-82-37.ec2.internal>
2021-05-20 13:32:19 +0000

    link_to_firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsHz1r9YxpV81KesDipZpTOXwPmUz89qY",
+  authDomain: "fb-sample-c7868.firebaseapp.com",
+  projectId: "fb-sample-c7868",
+  storageBucket: "fb-sample-c7868.appspot.com",
+  messagingSenderId: "1061416113989",
+  appId: "1:1061416113989:web:7ca16c01b805dfcb09502c",
+  measurementId: "G-XMXYQEN8MH"
+};
+
+firebase.initializeApp(firebaseConfig);
