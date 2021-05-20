0cfc512a24f363a80432a860b82430ec0381523b
EC2 Default User <ec2-user@ip-172-31-82-37.ec2.internal>
2021-05-20 14:18:39 +0000

    Save Avatar By Storage

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
-/* ライブラリ */
+
 import { firebase, auth, uiConfig } from "../lib/firebase";
 
 function Login() {
